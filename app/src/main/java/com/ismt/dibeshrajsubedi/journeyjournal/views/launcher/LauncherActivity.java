5e81207e63ee4db88c857b4570b53ca5f1048764
Dibesh Raj Subedi <dibeshrsubedi@gmail.com>
2022-03-07 21:44:09 +0545

    Splash Screen Launcher Implemented

--- app/src/main/java/com/ismt/dibeshrajsubedi/journeyjournal/views/launcher/LauncherActivity.java
+++ app/src/main/java/com/ismt/dibeshrajsubedi/journeyjournal/views/launcher/LauncherActivity.java
@@ -1,16 +1,42 @@
 package com.ismt.dibeshrajsubedi.journeyjournal.views.launcher;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import com.ismt.dibeshrajsubedi.journeyjournal.R;
+import com.ismt.dibeshrajsubedi.journeyjournal.views.activities.authentication.AuthenticationActivity;
 
+/**
+ * Application Entry Point Begins From Here, It Creates A Splash Screen And Redirect To Authentication Activity
+ */
 public class LauncherActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
+        ImageView imageView = findViewById(R.id.iv_splash_logo);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in);
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                startActivity(new Intent(LauncherActivity.this, AuthenticationActivity.class));
+                finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+        imageView.startAnimation(animation);
     }
 }
