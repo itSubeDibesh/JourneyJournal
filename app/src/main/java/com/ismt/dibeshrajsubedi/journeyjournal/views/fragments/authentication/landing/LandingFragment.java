00ea24415413205c60fadeba8d034642d95f3bcf
Dibesh Raj Subedi <dibeshrsubedi@gmail.com>
2022-03-08 00:19:24 +0545

    Landing Fragment Navigation Handled including onBackPressed event ✅

--- app/src/main/java/com/ismt/dibeshrajsubedi/journeyjournal/views/fragments/authentication/landing/LandingFragment.java
+++ app/src/main/java/com/ismt/dibeshrajsubedi/journeyjournal/views/fragments/authentication/landing/LandingFragment.java
@@ -1,66 +1,70 @@
 package com.ismt.dibeshrajsubedi.journeyjournal.views.fragments.authentication.landing;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.activity.OnBackPressedCallback;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import com.ismt.dibeshrajsubedi.journeyjournal.R;
+import com.ismt.dibeshrajsubedi.journeyjournal.views.components.ComponentsViewModel;
+
 
 /**
- * A simple {@link Fragment} subclass.
- * Use the {@link LandingFragment#newInstance} factory method to
- * create an instance of this fragment.
+ * Core Application Begins From This Fragment and Passes to Login or Register Fragment
  */
 public class LandingFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public LandingFragment() {
-        // Required empty public constructor
-    }
+    private ComponentsViewModel componentsViewModel;
+    private Button login, register;
+    private NavController navController;
 
     /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
+     * Extract Elements Globally
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LandingFragment.
+     * @param viewGroup ViewGroup
      */
-    // TODO: Rename and change types and number of parameters
-    public static LandingFragment newInstance(String param1, String param2) {
-        LandingFragment fragment = new LandingFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public void extractElements(ViewGroup viewGroup) {
+        login = viewGroup.findViewById(R.id.btn_login);
+        register = viewGroup.findViewById(R.id.btn_register);
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        componentsViewModel = new ViewModelProvider(this).get(ComponentsViewModel.class);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_landing, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ViewGroup viewGroup = (ViewGroup) inflater.inflate(R.layout.fragment_landing, container, false);
+        extractElements(viewGroup);
+        return viewGroup;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // Initialize Navigation Controller
+        navController = Navigation.findNavController(view);
+        // Navigate to Login
+        login.setOnClickListener(event -> navController.navigate(R.id.action_landingFragment_to_loginFragment));
+        // Navigate to Register
+        register.setOnClickListener(event -> navController.navigate(R.id.action_landingFragment_to_registerFragment));
+        // Handle Back pressed for exit confirmation
+        requireActivity().getOnBackPressedDispatcher().addCallback(getViewLifecycleOwner(), new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                componentsViewModel.exitConfirmation(requireActivity());
+            }
+        });
     }
 }
