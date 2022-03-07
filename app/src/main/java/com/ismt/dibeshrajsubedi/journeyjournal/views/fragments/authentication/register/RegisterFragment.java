8e5bf53de854307f1a8adec67273ce81513a2a81
Dibesh Raj Subedi <dibeshrsubedi@gmail.com>
2022-03-08 00:36:17 +0545

    Register Fragment Navigation Implemented 🗺

--- app/src/main/java/com/ismt/dibeshrajsubedi/journeyjournal/views/fragments/authentication/register/RegisterFragment.java
+++ app/src/main/java/com/ismt/dibeshrajsubedi/journeyjournal/views/fragments/authentication/register/RegisterFragment.java
@@ -1,66 +1,65 @@
 package com.ismt.dibeshrajsubedi.journeyjournal.views.fragments.authentication.register;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.ismt.dibeshrajsubedi.journeyjournal.R;
+import com.ismt.dibeshrajsubedi.journeyjournal.views.activities.home.HomeActivity;
 
 /**
- * A simple {@link Fragment} subclass.
- * Use the {@link RegisterFragment#newInstance} factory method to
- * create an instance of this fragment.
+ * Navigates to Home Activity or Login Fragment
  */
 public class RegisterFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private Button register;
+    private TextView login;
+    private NavController navController;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public RegisterFragment() {
-        // Required empty public constructor
-    }
 
     /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
+     * Extract Elements Globally
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RegisterFragment.
+     * @param viewGroup ViewGroup
      */
-    // TODO: Rename and change types and number of parameters
-    public static RegisterFragment newInstance(String param1, String param2) {
-        RegisterFragment fragment = new RegisterFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public void extractElements(ViewGroup viewGroup) {
+        login = viewGroup.findViewById(R.id.tv_login);
+        register = viewGroup.findViewById(R.id.btn_register);
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) { super.onCreate(savedInstanceState); }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ViewGroup viewGroup = (ViewGroup) inflater.inflate(R.layout.fragment_register, container, false);
+        extractElements(viewGroup);
+        return viewGroup;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // Initialize Navigation Controller
+        navController = Navigation.findNavController(view);
+        // Navigate to Home Fragment
+        register.setOnClickListener(event -> {
+            startActivity(new Intent(requireActivity(), HomeActivity.class));
+            requireActivity().finish();
+        });
+        // Navigate to Login
+        login.setOnClickListener(event -> navController.navigate(R.id.action_registerFragment_to_loginFragment));
     }
 }
